95c7803d1bfed93994cfc0a69881798cda527ceb
Aleksander Shevchuk <sheff1303@gmail.com>
2022-05-20 13:59:53 +0300

    script.js updated

--- js/sdript.js
+++ js/sdript.js
@@ -1,1 +1,19 @@
+const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
 
+const personalMoviDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("Один из просмотреных фильмов?", "");
+const b = prompt("на сколько оцените его?", "");
+const c = prompt("Один из просмотреных фильмов?", "");
+const d = prompt("На сколько оцените его?", "");
+
+personalMoviDB.movies[a] = b;
+personalMoviDB.movies[c] = d;
+
+console.log(personalMoviDB);
